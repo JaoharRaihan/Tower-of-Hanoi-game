503005ad211fed48a6b2dcedd07ce160ec989086
Masum Hasan <80238860+JaoharRaihan@users.noreply.github.com>
2023-03-12 23:49:41 +0600

    s

---
+++ app.js
@@ -0,0 +1,24 @@
+function oneto2() {
+    moveDisc(document.querySelector('#towerA'), document.querySelector('#towerB'));
+  }
+  
+  function twoto3() {
+    moveDisc(document.querySelector('#towerB'), document.querySelector('#towerC'));
+  }
+  
+  function threeto1() {
+    moveDisc(document.querySelector('#towerC'), document.querySelector('#towerA'));
+  }
+  
+  function threeto2() {
+    moveDisc(document.querySelector('#towerC'), document.querySelector('#towerB'));
+  }
+  
+  function twoto1() {
+    moveDisc(document.querySelector('#towerB'), document.querySelector('#towerA'));
+  }
+  
+  function oneto3() {
+    moveDisc(document.querySelector('#towerA'), document.querySelector('#towerC'));
+  }
+  
---
+++ c.js
@@ -0,0 +1,66 @@
+// Get the div elements where disks can be moved to
+const d1 = document.getElementById("d1");
+const d2 = document.getElementById("d2");
+const d3 = document.getElementById("d3");
+
+// Get the buttons for moving disks
+const button12 = document.querySelector("#div4 button:nth-of-type(1)");
+const button23 = document.querySelector("#div4 button:nth-of-type(2)");
+const button31 = document.querySelector("#div4 button:nth-of-type(3)");
+const button32 = document.querySelector("#div4 button:nth-of-type(4)");
+const button21 = document.querySelector("#div4 button:nth-of-type(5)");
+const button13 = document.querySelector("#div4 button:nth-of-type(6)");
+
+// Keep track of which disk is selected
+let selectedDisk = null;
+
+// Add click event listeners to disks to select them
+const disks = document.querySelectorAll(".div1 button");
+disks.forEach((disk) => {
+  disk.addEventListener("click", () => {
+    selectedDisk = disk;
+  });
+});
+
+// Add click event listeners to buttons to move disks
+button12.addEventListener("click", () => {
+  if (selectedDisk && d1.contains(selectedDisk)) {
+    d2.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
+
+button23.addEventListener("click", () => {
+  if (selectedDisk && d2.contains(selectedDisk)) {
+    d3.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
+
+button31.addEventListener("click", () => {
+  if (selectedDisk && d3.contains(selectedDisk)) {
+    d1.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
+
+button32.addEventListener("click", () => {
+  if (selectedDisk && d3.contains(selectedDisk)) {
+    d2.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
+
+button21.addEventListener("click", () => {
+  if (selectedDisk && d2.contains(selectedDisk)) {
+    d1.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
+
+button13.addEventListener("click", () => {
+  if (selectedDisk && d1.contains(selectedDisk)) {
+    d3.appendChild(selectedDisk);
+    selectedDisk = null;
+  }
+});
